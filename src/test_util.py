1528cb561d74d1330526c803e062c1d67d2324b9
Michael Kerrin <michael.kerrin@rapidratings.com>
2025-04-09 13:21:02 +0100

    Add flaky test to test report

--- src/test_util.py
+++ src/test_util.py
@@ -1,9 +1,12 @@
 import unittest
 
+import util
+
 class TestUtil(unittest.TestCase):
 
     def test_calc(self):
         assert True
 
-    def test_calc2(self):
-        assert False
+    def test_calc_issues(self):
+        result =  util.calc_something(2, 4)
+        assert result == 8
