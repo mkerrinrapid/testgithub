f863d4d97aeab4e1d85bea9290b07566a26547d8
Michael Kerrin <michael.kerrin@rapidratings.com>
2025-04-11 10:45:18 +0100

    Remove extra test

--- src/test_util.py
+++ src/test_util.py
@@ -7,9 +7,6 @@ class TestUtil(unittest.TestCase):
     def test_calc(self):
         assert True
 
-    def test_calc2(self):
-        assert True
-
     def test_calc_issues(self):
         result =  util.calc_something(2, 4)
         assert result == 8
