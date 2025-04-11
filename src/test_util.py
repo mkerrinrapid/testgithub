9c2454181b112c5cc53e802fd1a29db094f80689
Michael Kerrin <michael.kerrin@rapidratings.com>
2025-04-11 12:25:33 +0100

    Test reporter

--- src/test_util.py
+++ src/test_util.py
@@ -13,6 +13,9 @@ class TestUtil(unittest.TestCase):
     def test_erh2(self):
         assert True
 
+    def test_erh3(self):
+        assert True
+
     def test_calc_issues(self):
         result =  util.calc_something(2, 4)
         assert result == 8
