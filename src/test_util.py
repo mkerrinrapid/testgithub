9144df9a7ebdff8972252b5f38c164e6209d6a6a
Michael Kerrin <michael.kerrin@rapidratings.com>
2025-04-11 13:16:56 +0100

    New test

--- src/test_util.py
+++ src/test_util.py
@@ -16,6 +16,9 @@ class TestUtil(unittest.TestCase):
     def test_erh3(self):
         assert True
 
+    def test_erh4(self):
+        assert True
+
     def test_calc_issues(self):
         result =  util.calc_something(2, 4)
         assert result == 8
